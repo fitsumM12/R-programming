985d234f29d755ab2f1486dc735ec19b10da8d88
fitsumM12 <fitsummesfindejene@gmail.com>
2022-05-01 11:00:23 +0530

    update

---
+++ Basics/04_Variable assignment II.R
@@ -0,0 +1,18 @@
+"
+Variable assignment II
+Variables are great to perform arithmetic operations with. 
+In this assignment, we have defined a variable my_apples. 
+You want to define another variable called my_oranges and add these two together.
+
+my_apples + my_oranges
+"
+
+# Assign a value to the variables my_apples and my_oranges
+my_apples <- 5
+my_oranges <- 6
+
+# Add these two variables together and print the result
+my_apples + my_oranges
+
+# Create the variable my_fruit
+my_fruit <- my_apples + my_oranges
