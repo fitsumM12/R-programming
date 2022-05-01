f8bbae8dc5b872d8dfc772e968d239e907405c02
fitsumM12 <fitsummesfindejene@gmail.com>
2022-05-01 15:40:28 +0530

    update

---
+++ Chapter 4 - Correlation and Regression/01_Scatterplots.R
@@ -0,0 +1,5 @@
+"
+Scatterplots
+Saved in your console is a dataset called women which contains the height and weight of 15 women (try typing it into your console and press enter to have a look).
+Let's have a look at the relationship between height and weight through a scatterplot, using the R function plot(). The first argument of plot() is the x-axis coordinates, and the second argument is the y-axis coordinates.
+"
