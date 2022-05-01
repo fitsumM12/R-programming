1ae2ce71afd7b85a0adf0743347a4862cca0dd5f
fitsumM12 <fitsummesfindejene@gmail.com>
2022-05-01 10:46:55 +0530

    update

---
+++ Basics/Little arithmmetics with R.R
@@ -0,0 +1,32 @@
+"
+In its most basic form R can be used as a simple calculator. Consider the following arithmetic operators:
+
+Addition: +
+Subtraction: -
+Multiplication: *
+Division: /
+Exponentiation: ^
+Modulo: %%
+The last three might need some explaining:
+
+The ^ operator raises the number to its left to the power of the number to its right: 
+for example 3^2 is 9.
+The modulo returns the remainder of the division of the number to the left by the number on its right, for example 5 modulo 3 or 5 %% 3 is 2.
+With this knowledge, follow the instructions below to complete the exercise.
+"
+# Addition
+5 + 5 
+
+# Subtraction
+5 - 5 
+
+# Multiplication
+3 * 5
+
+# Division
+(5 + 5) / 2 
+
+# Exponentiation
+5^2
+# Modulo
+28 %% 6
